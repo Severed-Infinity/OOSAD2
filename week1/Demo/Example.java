57f17a8225d2b6ecb91b19647cf7d4317bcaa561
mint <myworld1991>
2015-01-29 12:36:18 +0000

    added visual paradigm java demo

---
+++ week1/Demo/Example.java
@@ -0,0 +1,56 @@
+package Demo;
+
+public class Example {
+
+	private string name = noname;
+	private long duration = 0;
+	private boolean initialized = false;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void getDuration() {
+		// TODO - implement Example.getDuration
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param duration
+	 */
+	public void setDuration(int duration) {
+		// TODO - implement Example.setDuration
+		throw new UnsupportedOperationException();
+	}
+
+	public void getInitalised() {
+		// TODO - implement Example.getInitalised
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param initalised
+	 */
+	public void setInitalised(int initalised) {
+		// TODO - implement Example.setInitalised
+		throw new UnsupportedOperationException();
+	}
+
+}
