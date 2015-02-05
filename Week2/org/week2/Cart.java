060bce7756e69551a11de4b9df0e56552639aa7e
Severed-Infinity <masterdavidswift@hotmail.com>
2015-02-05 12:44:11 +0000

    end of lab 2

--- Week2/org/week2/Cart.java
+++ Week2/org/week2/Cart.java
@@ -1,10 +1,15 @@
 package org.week2;
 
+import java.util.*;
+
 public class Cart {
 
 	public Cart() {
-		// TODO - implement Cart.Cart
-		throw new UnsupportedOperationException();
+		for(short i = 0; i < 100; i++){
+			cartItems.add(new CartItem(i, 10.50F, 3.45F));
+		}
 	}
+
+	protected Collection<CartItem> cartItems;
 
 }
--- Week2/org/week2/CartItem.java
+++ Week2/org/week2/CartItem.java
@@ -7,4 +7,30 @@ public class CartItem {
 		throw new UnsupportedOperationException();
 	}
 
+	public CartItem(short id, float price, float tax){
+		itemid = id;
+		itemPrice = price;
+		itemTaxBand = tax;
+	}
+
+	public short getItemid() {
+		return this.itemid;
+	}
+
+	public void setItemid(short itemid) {
+		this.itemid = itemid;
+	}
+
+	public float getItemPrice() {
+		return this.itemPrice;
+	}
+
+	public void setItemPrice(float itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	private short itemid = 0;
+	private float itemPrice = 0;
+	private float itemTaxBand = 0;
+
 }
